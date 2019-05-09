6255fb9f500be71e61a252259877dc4e78e50b33
Haekal Syahefiran <haekalsyahefiransmp7c@User1s-MacBook-Pro.local>
2019-05-09 09:32:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/haekalsyahefiransmp7c/alert/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.haekalsyahefiransmp7c.alert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.alert);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog() {
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+
+    }
+}
